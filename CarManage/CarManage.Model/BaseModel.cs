480d932dec5c3e6d52f9676124aa484e1481e1b2
wxll1120 <wxll1120@gmail.com>
2015-07-23 12:13:40 +0800

    为BaseModel新增PageIndex,PageSize,TotalCount,PageCount等属性

--- CarManage/CarManage.Model/BaseModel.cs
+++ CarManage/CarManage.Model/BaseModel.cs
@@ -2,7 +2,46 @@
 
 namespace CarManage.Model
 {
+    [Serializable]
     public class BaseModel
     {
+        public BaseModel()
+        {
+            PageIndex = 0;
+            PageSize = 20;
+            TotalCount = 0;
+        }
+
+        /// <summary>
+        /// 当前页码
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// 每页数量
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 总数
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// 页数
+        /// </summary>
+        public int PageCount
+        {
+            get
+            {
+                if (PageSize.Equals(0))
+                    return 0;
+
+                if (TotalCount % PageSize == 0)
+                    return TotalCount / PageSize;
+
+                return TotalCount / PageSize + 1;
+            }
+        }
     }
 }
